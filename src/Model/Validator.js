fcbee0e54f8d0d53479e31d50a7c58c14157019e
YEONSEO <yeonseochoi0717@gmail.com>
2024-11-07 18:10:03 +0900

    feat: checkFormat 메서드가 [상품,수량]을 값으로 가지는 배열을 반환하도록 수정

--- src/Model/Validator.js
+++ src/Model/Validator.js
@@ -1,21 +1,23 @@
 import { Console } from "@woowacourse/mission-utils";
 
 //개별 상품 사이 공백 허용x, 한글 상품명만 허용
-const PURCHASE_FORMAT = /^\[([ㄱ-ㅎ가-힣]+)-(\d+)](?:,\[([ㄱ-ㅎ가-힣]+)-(\d+)])*$/;
+const PURCHASES_FORMAT = /^\[([ㄱ-ㅎ가-힣]+)-(\d+)](?:,\[([ㄱ-ㅎ가-힣]+)-(\d+)])*$/;
+const PURCHASE_FORMAT = /\[([ㄱ-ㅎ가-힣]+)-(\d+)]/g;
 
 class Validator {
-    constructor(parameters) {}
+    constructor(parameters) { }
 
     purchaseInput(input) {
-        let validInput = this.checkFormat(input);
+        let formattedInput = this.checkFormat(input);
 
-        
+
     }
 
     checkFormat(input) {
-        if (PURCHASE_FORMAT.test(input)) {
-            const match = input.match(PURCHASE_FORMAT);
-            return match[0];
+        if (PURCHASES_FORMAT.test(input)) {
+            const matches = [...input.matchAll(PURCHASE_FORMAT)];
+
+            return matches.map(match => [match[1], Number(match[2])]);
         } else {
             const errorMessage = `[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.`; // OutputView로 빼기?
             throw new Error(errorMessage);
