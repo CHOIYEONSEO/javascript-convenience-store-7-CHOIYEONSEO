bc8d81160d674fa65fc9516d212747da31c99bd8
YEONSEO <yeonseochoi0717@gmail.com>
2024-11-07 17:11:17 +0900

    refactor: 올바른 형식으로 구매할 상품과 수량 입력했는지 확인하는 메서드 분리

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -3,17 +3,12 @@ import Validator from "../src/Model/Validator.js";
 const validate = new Validator();
 
 describe("Validator 테스트", () => {
-    test.each(["[사이다-2]", "[사이다-2],[감자칩-1]"])("구매하고 싶은 값 %s는 -, [], ,로 잘 구분되어 그 값을 반환한다.", () => {
-        const passInput = ["[사이다-2]", "[사이다-2],[감자칩-1]"];
-
-        expect(validate.purchaseInput(passInput[0])).toBe("[사이다-2]");
-        expect(validate.purchaseInput(passInput[1])).toBe("[사이다-2],[감자칩-1]");
+    test.each(["[사이다-2]", "[사이다-2],[감자칩-1]"])("구매하고 싶은 값 %s는 -, [], ,로 잘 구분되어 그 값을 반환한다.", (condition) => {
+        expect(validate.checkFormat(condition)).toBe(condition);
     });
 
-    test.each(["사이다", "2", "사이다-", "사이다-2", "[사이다]", "[사이다-2", "사이다2", "[사이다-2][감자칩-1]"])("구매하고 싶은 값 %s는 잘못된 형식으로 에러 처리한다.", (condition) => {
-        
-        expect(() => validate.purchaseInput(condition)).toThrow("[ERROR]");
-
+    test.each(["사이다", "2", "사이다-", "사이다-2", "[사이다]", "[사이다-2", "사이다2", "[사이다-2][감자칩-1]"])("구매하고 싶은 값 %s는 잘못된 형식으로 에러 처리한다.", (condition) => { 
+        expect(() => validate.checkFormat(condition)).toThrow("[ERROR]");
     });
 
 
--- src/Model/Validator.js
+++ src/Model/Validator.js
@@ -1,14 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 
+//개별 상품 사이 공백 허용x, 한글 상품명만 허용
+const PURCHASE_FORMAT = /^\[([ㄱ-ㅎ가-힣]+)-(\d+)](?:,\[([ㄱ-ㅎ가-힣]+)-(\d+)])*$/;
+
 class Validator {
     constructor(parameters) {}
 
     purchaseInput(input) {
+        let validInput = this.checkFormat(input);
 
-        //개별 상품 사이 공백 허용x, 한글 상품명만 허용
-        const regex = /^\[([ㄱ-ㅎ가-힣]+)-(\d+)](?:,\[([ㄱ-ㅎ가-힣]+)-(\d+)])*$/;
-        if (regex.test(input)) {
-            const match = input.match(regex);
+        
+    }
+
+    checkFormat(input) {
+        if (PURCHASE_FORMAT.test(input)) {
+            const match = input.match(PURCHASE_FORMAT);
             return match[0];
         } else {
             const errorMessage = `[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.`; // OutputView로 빼기?
