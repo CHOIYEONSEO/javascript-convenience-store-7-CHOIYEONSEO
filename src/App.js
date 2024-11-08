c222ef21f485ef9e307c6a5084f55fb4d2ed5703
YEONSEO <yeonseochoi0717@gmail.com>
2024-11-09 04:34:27 +0900

    feat: 프로모션을 public/promotions.md 파일 이용해 파일 입출력을 통해 불러옴

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Product from "./Model/Product.js";
 import {Console} from "@woowacourse/mission-utils";
 import Store from "./Controller/Store.js";
 import readFile from "./Model/readFile.js";
+//import promotionManage from "./Model/promotionManage.js";
 
 const PRODUCTS_FILE_PATH = "public/products.md";
+const PROMOTIONS_FILE_PATH = "public/promotions.md";
 const READ_OPTION = "utf-8";
 
 class App {
     async run() {
         const productsData = readFile(PRODUCTS_FILE_PATH, READ_OPTION);
+        const promotionsData = readFile(PROMOTIONS_FILE_PATH, READ_OPTION);
         const products = productsData.map((element) => {
             const splitElement = element.split(",");
             return new Product(...splitElement);
